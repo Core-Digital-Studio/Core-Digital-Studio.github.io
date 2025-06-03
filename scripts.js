40b2709a5ebc6365944ddb4d8916f9853710cf62
ty <76914455+tyhirn@users.noreply.github.com>
2025-06-03 10:23:18 -0400

    updated with logo

--- scripts.js
+++ scripts.js
@@ -1,29 +1,29 @@
 
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('form');
-    const successMessage = document.querySelector('.success');
-    const errorMessage = document.querySelector('.error');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const form = document.querySelector('form');
+//     const successMessage = document.querySelector('.success');
+//     const errorMessage = document.querySelector('.error');
 
-    form.addEventListener('submit', async function (e) {
-        e.preventDefault();
-        const formData = new FormData(form);
-        const response = await fetch(form.action, {
-            method: 'POST',
-            body: formData,
-            headers: {
-                'Accept': 'application/json'
-            }
-        });
+//     form.addEventListener('submit', async function (e) {
+//         e.preventDefault();
+//         const formData = new FormData(form);
+//         const response = await fetch(form.action, {
+//             method: 'POST',
+//             body: formData,
+//             headers: {
+//                 'Accept': 'application/json'
+//             }
+//         });
 
-        if (response.ok) {
-            form.reset();
-            successMessage.style.display = 'block';
-            errorMessage.style.display = 'none';
-        } else {
-            successMessage.style.display = 'none';
-            errorMessage.style.display = 'block';
-        }
-    });
-});
+//         if (response.ok) {
+//             form.reset();
+//             successMessage.style.display = 'block';
+//             errorMessage.style.display = 'none';
+//         } else {
+//             successMessage.style.display = 'none';
+//             errorMessage.style.display = 'block';
+//         }
+//     });
+// });
