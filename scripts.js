f1a97866788ab2809471f1410cb38ec6cb1c0605
ty <76914455+tyhirn@users.noreply.github.com>
2025-06-04 12:19:57 -0400

    done

--- scripts.js
+++ scripts.js
@@ -1,29 +1,14 @@
-
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     const form = document.querySelector('form');
-//     const successMessage = document.querySelector('.success');
-//     const errorMessage = document.querySelector('.error');
-
-//     form.addEventListener('submit', async function (e) {
-//         e.preventDefault();
-//         const formData = new FormData(form);
-//         const response = await fetch(form.action, {
-//             method: 'POST',
-//             body: formData,
-//             headers: {
-//                 'Accept': 'application/json'
-//             }
-//         });
-
-//         if (response.ok) {
-//             form.reset();
-//             successMessage.style.display = 'block';
-//             errorMessage.style.display = 'none';
-//         } else {
-//             successMessage.style.display = 'none';
-//             errorMessage.style.display = 'block';
-//         }
-//     });
-// });
+(function(d,t) {
+  var BASE_URL="https://app.chatwoot.com";
+  var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+  g.src=BASE_URL+"/packs/js/sdk.js";
+  g.defer = true;
+  g.async = true;
+  s.parentNode.insertBefore(g,s);
+  g.onload=function(){
+    window.chatwootSDK.run({
+      websiteToken: 'wNFs4K98GsFQDZJKPBZTW9tS',
+      baseUrl: BASE_URL
+    })
+  }
+})(document,"script");
